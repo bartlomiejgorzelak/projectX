f6a7856b2b4e9be9f49d4660a1dbdf56463441e2
bartlomiej.gorzelak@gmail.com <gorzelak1!>
2022-07-28 09:53:01 +0200

    add code

---
+++ ProjectX/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using ProjectX.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectX.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string name, string address)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Summary", new { name, address });
+            }
+            return View();
+        }
+
+        public ActionResult Summary(string name, string address)
+        {
+            User user = new User() { Name = name, Address = address };
+            return View(user);
+        }
+
+        public ActionResult SwitchCulture(bool culture)
+        {
+            if (culture != null)
+            {
+                if (culture)
+                {
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-GB");
+                    Session["culture"] = "en-GB";
+                }
+                else
+                {
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo("pl-PL");
+                    Session["culture"] = "pl-PL";
+                }
+            }
+            return View("index");
+        }
+    }
+}
---
+++ ProjectX/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectX.Models
+{
+    public class User
+    {
+        [Display(ResourceType = typeof(Resources.Resources), Name = "name")]
+        [Required(ErrorMessageResourceType = typeof(Resources.Resources), ErrorMessageResourceName = "required")]
+        [StringLength(30, ErrorMessageResourceType = typeof(Resources.Resources), ErrorMessageResourceName = "maxLength")]
+
+        public string Name { set; get; }
+
+        [Display(ResourceType = typeof(Resources.Resources), Name = "email")]
+        [Required(ErrorMessageResourceType = typeof(Resources.Resources), ErrorMessageResourceName = "required")]
+        [RegularExpression(@"^[_A-Za-z0-9-]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*(\.[A-Za-z]{2,4})$", ErrorMessageResourceType = typeof(Resources.Resources), ErrorMessageResourceName = "emailError")]
+        public string Address { set; get; }
+    }
+}
